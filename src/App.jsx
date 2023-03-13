7dd9f27ffdef3f79851f11caaf7fd80b9a75891a
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-03-13 11:45:42 +0530

    created pages and routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPass from "./pages/ForgotPass";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-pass" element={<ForgotPass />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <h1>
+        header
+      </h1>
+    </>
+  );
+};
+export default Header;
